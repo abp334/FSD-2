0768f957df1bf6b0dbb457d538a73f3597f45f5d
abp334 <timepass.5354@gmail.com>
2025-04-22 09:48:11 +0530

    Add files via upload

---
+++ T2/Program08.js
@@ -0,0 +1,12 @@
+/*
+Link HTML,CSS,JS
+1)app.use(path,callback)
+path - for which middleware function is called
+callback- to call series of middleware function
+express.static(root,[options]) //The middleware function which is already available
+2)path.join() //if file is available in different folders than this is called to join the path
+*/
+const expr = require("express");
+const app = expr();
+app.use(expr.static(__dirname));
+app.listen(5200);
