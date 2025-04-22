26bfce33864b139c6b547bdb2b1e69a15ee4891a
abp334 <timepass.5354@gmail.com>
2025-04-22 10:07:44 +0530

    Delete T2/Program09.js

--- T2/Program09.js
+++
@@ -1,5 +0,0 @@
-const expr = require("express");
-const app = expr();
-app.use(expr.static("Frontend"));
-app.listen(5001);
-//when HTML file is in another directory like in this the HTML file is in Frontend which is in EXPR.
